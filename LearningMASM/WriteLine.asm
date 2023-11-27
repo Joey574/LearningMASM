50715f8a5708e67d98bd6db5b6ec74acb1f93319
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-11-27 08:34:48 -0800

    lost, confused, hurt

--- LearningMASM/WriteLine.asm
+++ LearningMASM/WriteLine.asm
@@ -14,12 +14,12 @@ WriteLine PROC
     mov rcx, -11
     call    GetStdHandle    ; get stdout file handle
     mov rcx, rax            ; set as first parameter
-    mov rdx, offset newLine  ; set message address as second argument
-    mov r8, 2             ; length as third
+    mov rdx, offset newLine ; set message address as second argument
+    mov r8, 2               ; length as third
     mov r9, written         ; variable to write # bytes written
     call    WriteFile       ; prints message to console
 
 	ret
-	
+
 	WriteLine ENDP
 	END
