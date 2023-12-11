aa1e2f3eb67d571924346f2828acd6a9fe131275
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-12-10 19:32:55 -0800

    update science quiz to now address user

--- GS03_02_Science_Quiz/main.asm
+++ GS03_02_Science_Quiz/main.asm
@@ -2,16 +2,20 @@ extern Write: proc
 extern WriteLine: proc
 extern ReadLine: proc
 extern ConvertNumberToASCII: proc
+extern CopyArray: proc
 
 .data
 
 promptOne db "Please enter your name: "
- 
-inputPrompt db "Input (a,b,c,d): ",0
 
-qOne db "What is the speed of light?",13,10,"A) 272,182,455 m/s",13,10,"B) 152,999,982 m/s",13,10,"C) 305,542,123 m/s",13,10,"D) 299,792,458 m/s",0
-qTwo db "What is the largest object in the the solar system?",13,10,"A) Sun",13,10,"B) Saturn",13,10,"C) Earth",13,10,"D) Jupiter",0
-qThree db "In MASM x64, what register is used to pass the first argument?",13,10,"A) RAX",13,10,"B) R8", 13,10,"C) RCX",13,10,"D) R10",0
+userName db 256 dup(0)
+
+inputPrompt db "Input (a,b,c,d): ",0
+aP db ", "
+
+qOne db "what is the speed of light?",13,10,"A) 272,182,455 m/s",13,10,"B) 152,999,982 m/s",13,10,"C) 305,542,123 m/s",13,10,"D) 299,792,458 m/s",0
+qTwo db "what is the largest object in the the solar system?",13,10,"A) Sun",13,10,"B) Saturn",13,10,"C) Earth",13,10,"D) Jupiter",0
+qThree db "in MASM x64, what register is used to pass the first argument?",13,10,"A) RAX",13,10,"B) R8", 13,10,"C) RCX",13,10,"D) R10",0
 
 outputPromptOne db "You got ",0
 outputPromptTwo db "/3",0
@@ -25,6 +29,29 @@ main PROC
 	push rbp
 	mov rbp, rsp
 
+
+	; get name
+	mov rcx, offset promptOne
+	call Write
+
+	call ReadLine ; get name
+	push rcx ; store length
+	mov rcx, rax
+	mov rdx, offset userName ; destination array
+	call CopyArray ; copy source into userName
+	pop rcx ; retrieve length
+
+	; add ", " to end of name for output
+	mov rdx, offset userName
+	add rdx, rcx ; length offset
+	mov byte ptr [rdx], 0 ; remove new line
+	mov byte ptr [rdx-1], 0 ; remove new line
+	mov word ptr [rdx-2], " ,"
+
+
+	; QUIZ SECTION
+	mov rcx, offset userName
+	call Write
 	mov rcx, offset qOne
 	call WriteLine
 	mov rcx, offset inputPrompt
@@ -37,6 +64,8 @@ main PROC
 	inc score
 	skipA:
 
+	mov rcx, offset userName
+	call Write
 	mov rcx, offset qTwo
 	call WriteLine
 	mov rcx, offset inputPrompt
@@ -49,6 +78,8 @@ main PROC
 	inc score
 	skipB:
 
+	mov rcx, offset userName
+	call Write
 	mov rcx, offset qThree
 	call WriteLine
 	mov rcx, offset inputPrompt
