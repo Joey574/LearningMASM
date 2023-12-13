6c1210536557dea54c9c03eb959df645d9846b10
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-12-13 07:58:45 -0800

    basic outline of labels for choose your adventure

--- GS03_04_Choose_Your_Adventure/main.asm
+++ GS03_04_Choose_Your_Adventure/main.asm
@@ -31,6 +31,43 @@ main PROC
 	push rbp
 	mov rbp, rsp
 
+
+	P1:
+
+		P1A:
+		jmp complete
+
+		P1B:
+		jmp complete
+
+		P1C:
+		jmp complete
+
+
+	P2:
+
+		P2A:
+		jmp complete
+
+		P2B:
+		jmp complete
+
+		P2C:
+		jmp complete
+
+	P3:
+		
+		P3A:
+		jmp complete
+
+		P3B:
+		jmp complete
+
+		P3C:
+		jmp complete
+		
+
+	complete:
 	
 	mov rsp,rbp
     pop rbp
