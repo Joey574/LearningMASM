2880506f29666474e44578b238edc94166b607eb
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-01-03 21:56:30 -0800

    comments added

--- SelfModifyingTest/main.asm
+++ SelfModifyingTest/main.asm
@@ -3,11 +3,11 @@ MySegment segment read write execute
 main PROC
 	mov eax, 90
 
-	mov dword ptr MyLabel, 9005e883h
+	mov dword ptr MyLabel, 90 05 e8 83 h	; nop - 5 - subtract - I think
 
 	MyLabel:
 		add eax, 5
-		nop
+		nop ; nop so we don't overwrite ret instruction, which would've been included in the dword otherwise
 
 		ret
 
