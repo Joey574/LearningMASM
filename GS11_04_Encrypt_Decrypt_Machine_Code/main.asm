0a0275fa98053d6a68eb05dce80a8475a0ba917e
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-06-03 09:49:00 -0700

    more work macine code encrypt decrypt

--- GS11_04_Encrypt_Decrypt_Machine_Code/main.asm
+++ GS11_04_Encrypt_Decrypt_Machine_Code/main.asm
@@ -6,6 +6,17 @@ extern ConvertASCIIToNumber: proc
 
 MySegment segment read write execute
 
+intro db "Enter the string to encrypt: ",0
+instruct db "Enter the number of values to rotate by: ",0
+
+m1 db "The encrypted message is: ",0
+m2 db "The decrypted message is: ",0
+
+original db 256 dup(0)
+len dq ?
+encrypt db ?
+
+
 main PROC
 
 	; call write
@@ -23,14 +34,27 @@ main PROC
 	; call convertasciitonumber
 	; e8 ed af ff ff
 
+	; mov rcx, offset intro
+	; 48 b9 00 60 
+	; 34 56 f6 7f 00 00
+
 	push rbp
 	mov rbp, rsp
 
 	lea rcx, dataloop
 	nop
-	nop
+
+	mov byte ptr dataloop, 0e8h
+	mov dword ptr dataloop + 1, 0ffffb007h 
+
+	mov rcx, offset intro
 
 	dataloop:
+	nop
+	nop
+	nop
+	nop
+	nop
 
 
 	encryptloop:
